1faf086c3e979addd64751fc5cb988bdd416ccc3
Hamza Rahimy <rahimy.hamza@gmail.com>
2022-07-27 14:47:44 +0000

    Import project to replit and set up development environment

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
 require('dotenv').config()
 
+// Connect to mongoDb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 app.use(cors())
 app.use(express.static('public'))
+
+/* MongoDb schema and model definitions */
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+});
+const exerciseSchema = new mongoose.Schema({
+  user: userSchema,
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  durationMinutes: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: false,
+    default: new Date(Date.now())
+  }
+});
+const UserModel = mongoose.model('User', userSchema);
+const ExerciseModel = mongoose.model('Exercise', exerciseSchema);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
